e144f48c93cb0c109a4b2d3821919db0bdb6a767
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-01 17:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 
 let button=document.querySelector("#submit");
 let output=document.querySelector("#book-list");
+let form=document.querySelector("form-group");
 
-button.addEventListener("click",()=>{
+button.addEventListener("click",(e)=>{
+	e.preventDefault();
+	
 	let title=document.querySelector("#title").value;
 let author=document.querySelector("#author").value;
 let isbn=parseInt(document.querySelector("#isbn").value);
 
+	let tr=document.createElement('tr');
+	tr.innerHTML=`<td>${title}</td>
+	             <td>${author}</td>
+	               <td>${isbn}</td>
+				   <td><button class="delete">X</button></td>`
+
+
+	output.append(tr);
+	form.reset();
+  
 	
 })
