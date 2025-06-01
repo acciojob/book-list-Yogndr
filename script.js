85de44535e0bd1b8362789bdfdacf5eda2c824f9
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-01 15:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let button=document.querySelector("#submit");
+let output=document.querySelector("#book-list");
+
+button.addEventListener("click",()=>{
+	let title=document.querySelector("#title").value;
+let author=document.querySelector("#author").value;
+let isbn=parseInt(document.querySelector("#isbn").value);
+
+	
+})
